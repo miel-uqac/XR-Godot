b5f5ff001b8493ecfa23d0956e6e8c33fc5cdd61
library-rat <1ucas_z@protonmail.com>
2025-05-27 08:00:29 -0400

    cleaning depot for tutorial

--- qdvrtour/GameObjects/ColorChange.gd
+++ qdvrtour/GameObjects/ColorChange.gd
@@ -10,10 +10,13 @@ extends XRToolsPickable
 #Reference du mesh de la sphère
 @onready var mesh3D : MeshInstance3D = $MeshInstance3D
 #Maximum speed expected
-@export var maxSpeed : int = 100
+@export var maxSpeed : int = 1
 
 func _physics_process(delta: float) -> void:
+	#On récupère le matériau de la sphère
 	var material = mesh3D.mesh.surface_get_material(0)
-	var interpval = clamp(linear_velocity.length(), 0, maxSpeed)/maxSpeed
-	material.albedo_color = slowColor.lerp(fastColor, interpval)
+	#On va calculer la valeur utilisée dans l'interpolation à l'aide de la vitesse de la balle
+	var interp_value = clamp(linear_velocity.length(), 0, maxSpeed)/maxSpeed
+	#on change la couleur du materiau par l'interpolation entre les deux couleurs
+	material.albedo_color = slowColor.lerp(fastColor, interp_value)
 	
