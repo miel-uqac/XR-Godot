b5a445ad107c30e83bcae700806bc0c74c4fc703
library-rat <1ucas_z@protonmail.com>
2025-06-17 17:36:27 -0400

    changing the color lerp

--- qdvrtour/GameObjects/ColorChange.gd
+++ qdvrtour/GameObjects/ColorChange.gd
@@ -13,11 +13,17 @@ extends XRToolsPickable
 #Maximum speed expected
 @export var maxSpeed : int = 1
 
-func _physics_process(delta: float) -> void:
+func _physics_process(_delta: float) -> void:
 	#On récupère le matériau de la sphère
 	var material = mesh3D.mesh.surface_get_material(0)
 	#On va calculer la valeur utilisée dans l'interpolation à l'aide de la vitesse de la balle
 	var interp_value = clamp(linear_velocity.length(), 0, maxSpeed)/maxSpeed
 	#on change la couleur du materiau par l'interpolation entre les deux couleurs
-	material.albedo_color = Color(0,1,0).lerp(Color(1,0,0), interp_value)
-	
+	material.albedo_color = color_lerp(Color(0,1,0),Color(1,0,0), interp_value)
+
+
+
+
+func color_lerp(color1 : Color, color2 : Color, interp : float) -> Color :
+	var new_color = Vector3(color1.h, color1.s, color1.v).lerp(Vector3(color2.h, color2.s, color2.v), interp)
+	return Color.from_hsv(new_color.x, new_color.y, new_color.z)
